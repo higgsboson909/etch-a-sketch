3913b142c2a85f26e2f8457e04fd5d0ebfa2e959
higgsboson909 <higgsbosson906@gmail.com>
2024-09-27 21:03:56 +0500

    Create 16x16 elements of div dynamically

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,19 @@
-const div1
+
+
+const container = document.querySelector(".container");
+
+
+const rows = 16;
+const columns = 16;
+
+// for 16 by 16 square
+
+for(let i = 0; i < rows; i++){
+    
+    for(let j = 0; j < columns; j++){
+        let gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        container.appendChild(gridItem);        
+    }
+}
+
