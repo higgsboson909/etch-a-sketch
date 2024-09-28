14dd106998b5c9b79c6aeff3ced1268321051aa3
higgsboson909 <higgsbosson906@gmail.com>
2024-09-28 20:41:23 +0500

    Color the grid when mouse is on that pixel

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,25 +1,53 @@
 // refer the container 
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
+let rowItem;
+let size = 15;
+let columnItem;
 // default size of grid to 16
-let size = 30;
-
-for(let i = 0; i < size; i++){
+    for(let i = 0; i < size; i++){
     
-    // create i no. of rows inside the container
-    const rowItem = document.createElement("div");
-    rowItem.classList.add("row-item");
-
-    // append these rows in the container
-    container.appendChild(rowItem);
-
-
-    // create j no. of columns for each row
-    for(let j = 0; j < size; j++){
-        const columnItem = document.createElement("div");
-        columnItem.classList.add("column-item");
+        // create i no. of rows inside the container
+        rowItem = document.createElement("div");
+        rowItem.classList.add("row-item");
     
-        // append the columns inside each i row
-        rowItem.appendChild(columnItem);
+        // append these rows in the container
+        container.appendChild(rowItem);
+    
+    
+        // create j no. of columns for each row
+        for(let j = 0; j < size; j++){
+            columnItem = document.createElement("div");
+            columnItem.classList.add("column-item");
+        
+            // append the columns inside each i row
+            rowItem.appendChild(columnItem);
+        }
     }
-}
+    
+    // select all boxes
+    const columns = document.querySelectorAll(".column-item")
+    
+    columns.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.style.cssText = "background-Color: red;";
+        });
+    });
+
+button.addEventListener('click', () => {
+
+
+
+    do{
+        s = prompt("Change the no. of squares per side: ");
+        s = +s;
+        if(isNaN(s) || s > 100 || s < 0){
+            alert("Number must be b/w 1 and 100\nTry Agian");
+        }
+    }while(isNaN(s) || s > 100 || s < 1);    
+
+
+
+    
+});
